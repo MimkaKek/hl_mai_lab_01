c8e0ebc18a02074fb80b6dae631fb5228a125551
MimkaKek <grislava@gmail.com>
2024-03-30 19:07:39 +0300

    fin

--- init_psql.py
+++ init_psql.py
@@ -10,17 +10,15 @@ def init_users() -> None:
     for user in users:
         resp = requests.post(f'http://localhost:8080/user?first_name={user["first_name"]}&last_name={user["last_name"]}&email={user["email"]}&title={user["title"]}&login={user["login"]}&password={user["password"]}')
         print(resp.status_code)
-        print(resp.text)
 
 def init_paths() -> None:
     paths = get_json_arr('paths_init.json')
     for path in paths:
         resp = requests.post(f'http://localhost:8080/path?startpoint={path["startpoint"]}&endpoint={path["endpoint"]}')
-        print(f'http://localhost:8080/path?startpoint={path["startpoint"]}&endpoint={path["endpoint"]}')
         print(resp.status_code)
 
 def main() -> None:
-    # init_users()
+    init_users()
     init_paths()
 
 if __name__ == '__main__':
